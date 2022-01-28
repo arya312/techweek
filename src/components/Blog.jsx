ba556bd92b911ef307db49a4fc7d995c5590955a
saujanyasood <saujanyaasood@gmail.com>
2022-01-29 01:47:09 +0530

    remove errors

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -21,7 +21,6 @@ function Blog({ image, title, subTitle, variants, animate }) {
         <div className="subTitle">
           <p>{subTitle}</p>
         </div>
-<<<<<<< HEAD
         <a
           href="https://bit.ly/ieee-techweek22"
           style={{ textDecoration: "none" }}
@@ -33,19 +32,6 @@ function Blog({ image, title, subTitle, variants, animate }) {
             color="inverse"
             icon={<HiOutlineArrowNarrowRight />}
           />
-=======
-        <a 
-        href = "http://bit.ly/ieee-techweek22"
-        style = {{ textDecoration: "none"}}
-        target = "_blank"
-        rel = "nonreferrer"
-        >
-          <Button
-          content="Read more"
-          color="inverse"
-          icon={<HiOutlineArrowNarrowRight />}
-        />
->>>>>>> 8907a601402f730306324746fa687ce07136c6f8
         </a>
       </div>
     </motion.div>
