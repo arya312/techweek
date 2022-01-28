19798e6c406c7d549053bf83ecd334f8fb7a70bb
Arya Pandey <aryapandey312@gmail.com>
2022-01-29 00:24:33 +0530

    events images commit

--- src/sections/Blogs.jsx
+++ src/sections/Blogs.jsx
@@ -4,9 +4,16 @@ import Button from "../components/Button";
 import Title from "../components/Title";
 import { useScroll } from "../components/useScroll";
 import { motion } from "framer-motion";
-import BlogImage1 from "../assets/blogImage1.jpg";
-import BlogImage2 from "../assets/blogImage2.jpg";
-import BlogImage3 from "../assets/blogImage3.jpg";
+import BlogImage1 from "../sections/eventsImages/basicprogramming.jpeg";
+import BlogImage2 from "../sections/eventsImages/circuits.jpeg";
+import BlogImage3 from "../sections/eventsImages/dsa.jpeg";
+import BlogImage4 from "../sections/eventsImages/flutter.jpeg";
+import BlogImage5 from "../sections/eventsImages/graphicdesign.png";
+import BlogImage6 from "../sections/eventsImages/iot.jpeg";
+import BlogImage7 from "../sections/eventsImages/ml.jpeg";
+import BlogImage8 from "../sections/eventsImages/robotics.jpeg";
+import BlogImage9 from "../sections/eventsImages/solidworks.png";
+import BlogImage10 from "../sections/eventsImages/webd.jpeg";
 import "../styles/sections/Blogs.scss";
 import { textAnimation, cardAnimation } from "../utils/Animations";
 
@@ -50,42 +57,49 @@ function Blogs() {
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage4}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage5}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage6}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage7}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage8}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
             animate={controls}
           />
           <Blog
-            image={BlogImage3}
+            image={BlogImage9}
+            title="How to Build a Strong Remote Work Culture"
+            subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
+            variants={cardAnimation}
+            animate={controls}
+          />
+          <Blog
+            image={BlogImage10}
             title="How to Build a Strong Remote Work Culture"
             subTitle="Kishan Sheth is the compnay VP of Talent Operations responsible for matching some of the world's greatest freelancers with companies who..."
             variants={cardAnimation}
